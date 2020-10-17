a43e032bbaed59d1bf39ca1be1eb15f1bc501081
Sara Cemal <saracemal@gmail.com>
2020-10-17 04:51:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritchie][:known_for] = 
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+
+programmer_hash
 end
